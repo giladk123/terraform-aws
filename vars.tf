a8cacfff6aa38d435a7ab42a9804aa0737d62593
Gilad Kalev <giladk@gmail.com>
2022-12-20 12:14:12 +0200

    v1

---
+++ vars.tf
@@ -0,0 +1,97 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0a6b2839d44d781b2"
+    us-east-2 = "ami-0ada6d94f396377f2"
+    us-west-1 = "ami-0f4feb99425e13b50"
+  }
+}
+
+variable "PRIV_KEY_PATH" {
+  default = "vprofilekey"
+}
+
+variable "PUB_KEY_PATH" {
+  default = "vprofilekey.pub"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "MYIP" {
+  default = "183.83.39.124/32"
+}
+
+variable "rmquser" {
+  default = "rabbit"
+}
+
+variable "rmqpass" {
+  default = "Gr33n@pple1324356"
+}
+
+variable "dbuser" {
+  default = "admin"
+}
+
+variable "dbpass" {
+  default = "admin123"
+}
+
+variable "dbname" {
+  default = "accounts"
+}
+
+variable "instance_count" {
+  default = "1"
+}
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "Zone1" {
+  default = "us-east-1a"
+}
+
+variable "Zone2" {
+  default = "us-east-1b"
+}
+
+variable "Zone3" {
+  default = "us-east-1c"
+}
+
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
+}
+
